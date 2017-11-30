13fa1856f79f460d8784c475bc76b00b3e934e9f
Yu <yliu@talend.com>
2017-11-30 10:48:05 +0100

    feature(TBD-5415) Add GCS User Agent String in Constant class

--- main/plugins/org.talend.librariesmanager/resources/java/routines/system/Constant.java
+++ main/plugins/org.talend.librariesmanager/resources/java/routines/system/Constant.java
@@ -8,14 +8,20 @@ package routines.system;
  */
 public abstract class Constant {
 
+    private static final String version_studio = org.talend.commons.utils.VersionUtils.getDisplayVersion();
+    
     /**
      * the default pattern for date parse and format
      */
     public static final String dateDefaultPattern = "dd-MM-yyyy";
 
     /**
-     * the default user agent string for AWS components
+     * the default user agent string for AWS and Azure components
      */
-    public static final String TALEND_USER_AGENT = "APN/1.0 Talend/6.4 Studio/6.4";
-
+    public static final String TALEND_USER_AGENT = "APN/1.0 Talend/" + version_studio + " Studio/" + version_studio;
+    
+    /**
+     * the default user agent string for GCS components
+     */
+    public static final String TALEND_USER_AGENT_GCS = "Studio/" + version_studio + " (GPN:Talend) DataIntegration/" + version_studio + " Jets3t/0.9.1";
 }
